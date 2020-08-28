509ff28fc833d4cb5ba5dc91c37b3b4ee0fb31cc
MichaelMayor <michael.davidge.mayor@gmail.com>
2020-08-28 14:17:13 -0400

    added base quiz answer checking functionality

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,89 @@
-var homeBtn = document.getElementById("#home-button");
-var resetBtn = document.getElementById("#reset-button");
-var startBtn = document.getElementById("#start-button");
-var tbody = document.getElementById('#tbody');
-var timer = document.getElementById('#timer');
-var question = document.getElementById('#question-text');
-var answers = document.getElementById('#answers');
-var result = document.getElementById('#result');
-var highscoreBtn = document.getElementById('#highscoreLink')
+var homeBtn = document.querySelector("#home-button");
+var resetBtn = document.querySelector("#reset-button");
+var startBtn = document.querySelector("#start-button");
+var tbody = document.querySelector('#tbody');
+var timer = document.querySelector('#timer');
+var questionText = document.querySelector('#question-text');
+var answers = document.querySelector('#answers');
+var answer1 = document.querySelector('#ans1');
+var answer2 = document.querySelector('#ans2');
+var answer3 = document.querySelector('#ans3');
+var answer4 = document.querySelector('#ans4');
+var result = document.querySelector('#result');
+var highscoreBtn = document.querySelector('#highscoreLink');
+var initials = document.querySelector('#initials');
 
 
-var questions = 0;
+
+
 var totalSeconds = 60;
 var secondsElapsed = 0;
+var currentQ = 0;
+var correctAnswer;
 var interval;
+var score;
+var question1 = {"Question":"Question Text1","Answers": ["Answer1","Answer2","Answer3","Answer4"], "Correct":1};
+var question2 = {"Question":"Question Text2","Answers": ["Answer1","Answer2","Answer3","Answer4"], "Correct":1};
+var question3 = {"Question":"Question Text3","Answers": ["Answer1","Answer2","Answer3","Answer4"], "Correct":1};
+var question4 = {"Question":"Question Text4","Answers": ["Answer1","Answer2","Answer3","Answer4"], "Correct":1};
+var question5 = {"Question":"Question Text5","Answers": ["Answer1","Answer2","Answer3","Answer4"], "Correct":1};
+var question6 = {"Question":"Question Text6","Answers": ["Answer1","Answer2","Answer3","Answer4"], "Correct":1};
+var question7 = {"Question":"Question Text7","Answers": ["Answer1","Answer2","Answer3","Answer4"], "Correct":1};
+var question8 = {"Question":"Question Text8","Answers": ["Answer1","Answer2","Answer3","Answer4"], "Correct":1};
+var question9 = {"Question":"Question Text9","Answers": ["Answer1","Answer2","Answer3","Answer4"], "Correct":1};
+var question10 = {"Question":"Question Text10","Answers": ["Answer1","Answer2","Answer3","Answer4"], "Correct":1};
+var questions = [question1,question2,question3,question4,question5,question6,question7,question8,question9,question10]
 
 
 function startTimer() {
-  // Write code to start the timer here
-  
   interval = setInterval(function(){
     var secondsRemaining = totalSeconds - secondsElapsed; 
-    timer.textContent = secondsRemaining
+    timer.textContent = secondsRemaining;
     secondsElapsed++;
-    if (secondsRemaining == 0) {clearInterval(interval)}
+    if (secondsRemaining == 0) {
+        quizEnd();
+    }
 }, 1000)}
 
-function quizSet(){}
+function quizSet(currentQ){
+    questionText.textContent = questions[currentQ].Question;
+    answer1.textContent = "1: " + questions[currentQ].Answers[0];
+    answer2.textContent = "2: " + questions[currentQ].Answers[1];
+    answer3.textContent = "3: " + questions[currentQ].Answers[2];
+    answer4.textContent = "4: " + questions[currentQ].Answers[3];
+    return questions[currentQ].Correct
 
-function quizBegin(){}
 
-function answerQuestion(){}
+}
 
+function quizStart(){
+    startTimer();
+    correctAnswer = quizSet(currentQ);
+}
+
+function answerQuestion(){
+    if(event.target.matches("button")) {
+        currentAns = parseInt(event.target.id[3]);
+        if (currentAns == correctAnswer){
+            result.textContent = "Correct!"
+            score = score + 10;
+        }
+        else {
+            result.textContent = "Incorrect!";
+            secondsElapsed = secondsElapsed + 5;
+        }
+        currentQ++;
+        if (currentQ<questions.length) {
+        quizSet(currentQ);}
+        else {
+            quizEnd();
+        }
+      }
+}
+
+function quizEnd(){
+    
+}
 function writeHighscores(){
     for (var i = 0; i < obj.length; i++) {
     var tr = "<tr>";
@@ -52,11 +105,14 @@ function goHome(){}
 
 function goHighscores(){}
 
+function setNewScore(){}
 
 
-homeBtn.addEventListener("click", goHome)
 
-startBtn.addEventListener("click", quizBegin);
+homeBtn.addEventListener("click", goHome);
+startBtn.addEventListener("click", quizStart);
+
+initials.addEventListener("submit", setNewScore);
 resetBtn.addEventListener("click", resetScores);
 answers.addEventListener("click", answerQuestion);
 highscoreBtn.addEventListener("click", goHighscores);
