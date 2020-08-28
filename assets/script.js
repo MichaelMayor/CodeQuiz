0c0232341221d26ccc8836c1f6039de69f0d6e0b
MichaelMayor <michael.davidge.mayor@gmail.com>
2020-08-28 12:57:10 -0400

    finished html

--- assets/script.js
+++ assets/script.js
@@ -3,18 +3,53 @@ var resetBtn = document.getElementById("#reset-button");
 var startBtn = document.getElementById("#start-button");
 var tbody = document.getElementById('#tbody');
 var timer = document.getElementById('#timer');
+var question = document.getElementById('#question-text');
+var answers = document.getElementById('#answers');
+var result = document.getElementById('#result');
 
 
-for (var i = 0; i < obj.length; i++) {
-    var tr = "<tr>";
 
-    /* Verification to add the last decimal 0 */
-    if (obj[i].value.toString().substring(obj[i].value.toString().indexOf('.'), obj[i].value.toString().length) < 2) 
-        obj[i].value += "0";
+var totalSeconds = 60;
+var secondsElapsed = 0;
+var interval;
 
-    /* Must not forget the $ sign */
-    tr += "<td>" + obj[i].key + "</td>" + "<td>$" + obj[i].value.toString() + "</td></tr>";
 
-    /* We add the table row to the table body */
-    tbody.innerHTML += tr;
-}
+function startTimer() {
+  // Write code to start the timer here
+  
+  interval = setInterval(function(){
+    var secondsRemaining = totalSeconds - secondsElapsed; 
+    timer.textContent = secondsRemaining
+    secondsElapsed++;
+    if (secondsRemaining == 0) {clearInterval(interval)}
+}, 1000)}
+
+function quizSet(){}
+
+function quizBegin(){}
+
+function goHome(){}
+
+function resetScores(){}
+
+
+
+    for (var i = 0; i < obj.length; i++) {
+        var tr = "<tr>";
+    
+        /* Verification to add the last decimal 0 */
+        if (obj[i].value.toString().substring(obj[i].value.toString().indexOf('.'), obj[i].value.toString().length) < 2) 
+            obj[i].value += "0";
+    
+        /* Must not forget the $ sign */
+        tr += "<td>" + obj[i].key + "</td>" + "<td>$" + obj[i].value.toString() + "</td></tr>";
+    
+        /* We add the table row to the table body */
+        tbody.innerHTML += tr;
+    }
+
+homeBtn.addEventListener("click", goHome)
+
+startBtn.addEventListener("click", quizBegin);
+resetBtn.addEventListener("click", resetScores);
+answers.addEventListener("click", answerQuestion);
