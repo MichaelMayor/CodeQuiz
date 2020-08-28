575caf0aa8e6c1fa5198ad420aaaae06a4562f4d
MichaelMayor <michael.davidge.mayor@gmail.com>
2020-08-28 13:11:26 -0400

    removed separate highscore and questions pages git push

--- assets/script.js
+++ assets/script.js
@@ -6,9 +6,10 @@ var timer = document.getElementById('#timer');
 var question = document.getElementById('#question-text');
 var answers = document.getElementById('#answers');
 var result = document.getElementById('#result');
+var highscoreBtn = document.getElementById('#highscoreLink')
 
 
-
+var questions = 0;
 var totalSeconds = 60;
 var secondsElapsed = 0;
 var interval;
@@ -28,28 +29,34 @@ function quizSet(){}
 
 function quizBegin(){}
 
-function goHome(){}
+function answerQuestion(){}
+
+function writeHighscores(){
+    for (var i = 0; i < obj.length; i++) {
+    var tr = "<tr>";
+
+    /* Verification to add the last decimal 0 */
+    if (obj[i].value.toString().substring(obj[i].value.toString().indexOf('.'), obj[i].value.toString().length) < 2) 
+        obj[i].value += "0";
+
+    /* Must not forget the $ sign */
+    tr += "<td>" + obj[i].key + "</td>" + "<td>$" + obj[i].value.toString() + "</td></tr>";
+
+    /* We add the table row to the table body */
+    tbody.innerHTML += tr;
+}}
 
 function resetScores(){}
 
+function goHome(){}
+
+function goHighscores(){}
 
 
-    for (var i = 0; i < obj.length; i++) {
-        var tr = "<tr>";
-    
-        /* Verification to add the last decimal 0 */
-        if (obj[i].value.toString().substring(obj[i].value.toString().indexOf('.'), obj[i].value.toString().length) < 2) 
-            obj[i].value += "0";
-    
-        /* Must not forget the $ sign */
-        tr += "<td>" + obj[i].key + "</td>" + "<td>$" + obj[i].value.toString() + "</td></tr>";
-    
-        /* We add the table row to the table body */
-        tbody.innerHTML += tr;
-    }
 
 homeBtn.addEventListener("click", goHome)
 
 startBtn.addEventListener("click", quizBegin);
 resetBtn.addEventListener("click", resetScores);
 answers.addEventListener("click", answerQuestion);
+highscoreBtn.addEventListener("click", goHighscores);
